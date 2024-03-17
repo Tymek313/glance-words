006920187b3cb720968b1ab54f7f8afc35ba13b0
Tymek313 <236069118+Tymek313@users.noreply.github.com>
2024-03-17 22:16:39 +0100

    Apply theme colors to ui components, add padding to list elements

--- app/src/main/java/com/example/glancewords/widget/ui/WordsWidget.kt
+++ app/src/main/java/com/example/glancewords/widget/ui/WordsWidget.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.produceState
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.glance.Button
+import androidx.glance.ColorFilter
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
@@ -17,6 +18,7 @@ import androidx.glance.action.ActionParameters
 import androidx.glance.appwidget.CircularProgressIndicator
 import androidx.glance.appwidget.action.ActionCallback
 import androidx.glance.appwidget.action.actionRunCallback
+import androidx.glance.appwidget.appWidgetBackground
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.lazy.items
 import androidx.glance.background
@@ -29,12 +31,12 @@ import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
 import androidx.glance.text.Text
 import androidx.glance.text.TextDefaults
+import androidx.glance.text.TextStyle
 import com.example.glancewords.R
 import com.example.glancewords.repository.CachingWordsRepository
 import com.example.glancewords.widget.WordsWidget
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.io.File
 
 @Composable
 fun WordsWidget() {
@@ -47,8 +49,8 @@ fun WordsWidget() {
 fun WordsWidgetContent() {
     val widgetState by loadWidgetState(LocalContext.current)
 
-    Column(GlanceModifier.fillMaxSize().background(GlanceTheme.colors.background).padding(8.dp)) {
-        Box(contentAlignment = Alignment.Center, modifier = GlanceModifier.defaultWeight()) {
+    Column(GlanceModifier.fillMaxSize().appWidgetBackground().background(GlanceTheme.colors.widgetBackground).padding(8.dp)) {
+        Box(contentAlignment = Alignment.Center, modifier = GlanceModifier.fillMaxWidth().defaultWeight().padding(bottom = 8.dp)) {
             when (val state = widgetState) {
                 WidgetState.InProgress -> CircularProgressIndicator()
                 WidgetState.Failure -> WordsText("Words file not found")
@@ -74,32 +76,38 @@ private fun loadWidgetState(context: Context): State<WidgetState> {
     }
 }
 
-
 @Composable
 private fun WordList(words: List<Pair<String, String>>, modifier: GlanceModifier = GlanceModifier) {
     LazyColumn(modifier) {
         items(words) { (englishWord, polishWord) ->
-            Row(GlanceModifier.fillMaxWidth().background(ImageProvider(R.drawable.rounded_background))) {
+            Row(
+                modifier = GlanceModifier
+                    .fillMaxWidth()
+                    .background(imageProvider = ImageProvider(R.drawable.rounded_background), colorFilter = ColorFilter.tint(GlanceTheme.colors.surface))
+                    .padding(12.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 val rowModifier = GlanceModifier.defaultWeight()
                     // Without setting padding and background glance puts them on some items (bug)
                     .padding(0.dp)
                     .background(Color.Transparent)
-                WordsText(text = englishWord, rowModifier)
-                WordsText(text = polishWord, rowModifier)
+                val style = TextDefaults.defaultTextStyle.copy(color = GlanceTheme.colors.onSurface)
+                WordsText(text = englishWord, rowModifier, style)
+                WordsText(text = polishWord, rowModifier, style)
             }
         }
     }
 }
 
 private sealed interface WidgetState {
-    object InProgress : WidgetState
-    object Failure : WidgetState
+    data object InProgress : WidgetState
+    data object Failure : WidgetState
     class Success(val words: List<Pair<String, String>>) : WidgetState
 }
 
 @Composable
-fun WordsText(text: String, modifier: GlanceModifier = GlanceModifier, maxLines: Int = Int.MAX_VALUE) {
-    Text(text, modifier, style = TextDefaults.defaultTextStyle.copy(color = GlanceTheme.colors.onBackground), maxLines)
+fun WordsText(text: String, modifier: GlanceModifier = GlanceModifier, style: TextStyle = TextDefaults.defaultTextStyle, maxLines: Int = Int.MAX_VALUE) {
+    Text(text, modifier, style = style.copy(color = GlanceTheme.colors.onBackground), maxLines)
 }
 
 class UpdateWidget : ActionCallback {
